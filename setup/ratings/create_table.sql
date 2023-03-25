7cf377e53ae896b7f2442ea15d11b1d721c10914
Juan Henao <jhenao@truora.com>
2023-03-25 18:09:14 -0500

    convert to unix to normal timestamp

--- setup/ratings/create_table.sql
+++ setup/ratings/create_table.sql
@@ -3,3 +3,16 @@ CREATE TABLE RATINGS ( user_id NUMBER(38),
 movie_id NUMBER(38),
 rating FLOAT,
 timestamp NUMBER(38));
+
+-- once loaded the csv data
+
+-- convert unix timestamp to date and time values
+ALTER TABLE RATINGS ADD (DATETIME TIMESTAMP);
+
+UPDATE RATINGS SET DATETIME = TO_TIMESTAMP(
+                TO_CHAR( 
+                TO_DATE( '1970-01-01', 'YYYY-MM-DD' ) + numtodsinterval( TIMESTAMP, 'SECOND' ), 
+                'YYYY-MM-DD HH24:MI:SS'
+                ), 'YYYY-MM-DD HH24:MI:SS');
+                
+ALTER TABLE RATINGS DROP COLUMN TIMESTAMP;
--- setup/tags/create_table.sql
+++ setup/tags/create_table.sql
@@ -3,3 +3,16 @@ CREATE TABLE TAGS ( user_id NUMBER(38),
 movie_id NUMBER(38),
 tag VARCHAR2(128),
 timestamp NUMBER(38));
+
+-- once loaded the data
+
+-- convert unix timestamp to date and time values
+ALTER TABLE TAGS ADD (DATETIME TIMESTAMP);
+
+UPDATE TAGS SET DATETIME = TO_TIMESTAMP(
+                TO_CHAR( 
+                TO_DATE( '1970-01-01', 'YYYY-MM-DD' ) + numtodsinterval( TIMESTAMP, 'SECOND' ), 
+                'YYYY-MM-DD HH24:MI:SS'
+                ), 'YYYY-MM-DD HH24:MI:SS');
+            
+ALTER TABLE TAGS DROP COLUMN TIMESTAMP;
